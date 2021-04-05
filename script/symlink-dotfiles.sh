638a873557ff22f9f40b15dcd69d3e2825e82ad7
tavo <tavo.annus@gmail.com>
2021-04-05 19:06:56 +0300

    Add tmux xonfig

--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -1,2 +1,3 @@
 ln -s -f ~/git/kilpkonn-settings/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/git/kilpkonn-settings/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s -f ~/git/kilpkonn-settings/config/tmux/.tmux.conf ~/.tmux.conf
