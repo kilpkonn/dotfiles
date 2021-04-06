b44ca30d90dae976deaae070cc18f8902424452b
tavo <tavo.annus@gmail.com>
2021-04-06 10:22:37 +0300

    Add procs config

--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -1,3 +1,4 @@
 ln -s -f ~/git/kilpkonn-settings/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/git/kilpkonn-settings/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s -f ~/git/kilpkonn-settings/config/tmux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/git/kilpkonn-settings/config/procs/config.toml ~/.config/procs/config.toml
