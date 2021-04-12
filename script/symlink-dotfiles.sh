9694beeda182ed3a1cd53a2dbc42d38fd5103c42
tavo <tavo.annus@gmail.com>
2021-04-12 18:18:08 +0300

    Add starship config

--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -2,3 +2,4 @@ ln -s -f ~/git/kilpkonn-settings/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/git/kilpkonn-settings/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s -f ~/git/kilpkonn-settings/config/tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/git/kilpkonn-settings/config/procs/config.toml ~/.config/procs/config.toml
+ln -s -f ~/git/kilpkonn-settings/config/starship/starship.toml ~/.config/starship.toml
