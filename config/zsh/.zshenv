a8175c517e09768b1261b9ddf26304441035e6ff
tavo <tavo.annus@gmail.com>
2021-04-20 16:35:47 +0300

    Add zsh

---
+++ config/zsh/.zshenv
@@ -0,0 +1,10 @@
+
+# Default programs
+export EDITOR="nvim"
+
+
+# ~/ Clean up
+export ZDOTDIR="$HOME/.config/zsh"
+
+# Path
+export PATH="$PATH:~/.idris2/bin"
--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -3,3 +3,5 @@ ln -s -f ~/git/kilpkonn-settings/config/alacritty/alacritty.yml ~/.config/alacri
 ln -s -f ~/git/kilpkonn-settings/config/tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/git/kilpkonn-settings/config/procs/config.toml ~/.config/procs/config.toml
 ln -s -f ~/git/kilpkonn-settings/config/starship/starship.toml ~/.config/starship.toml
+ln -s -f ~/git/kilpkonn-settings/config/zsh/.zshrc ~/.config/zsh/.zshrc
+ln -s -f ~/git/kilpkonn-settings/config/zsh/.zshenv ~/.zshenv
