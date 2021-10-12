13adc6e3ddaec6fe1e68a4bd75c56bc1bdae6c7d
Tavo Annus <tavo.annus@gmail.com>
2021-10-12 11:44:23 +0300

    Add img resize to lock

--- script/lock.sh
+++ script/lock.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bg="$(ls /usr/share/backgrounds/ | shuf -n 1)" && i3lock -i "/usr/share/backgrounds/${bg}"
+bg_img="/usr/share/backgrounds/$(ls /usr/share/backgrounds/ | shuf -n 1)";
+screen_dimensions="$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')"
+
+ convert $bg_img -resize ${screen_dimensions} RGB:- | i3lock --raw ${screen_dimensions}:rgb --image /dev/stdin
+
