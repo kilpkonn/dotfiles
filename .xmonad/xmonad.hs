45fe67f5c2d50aeb5a89569984d259c450c7bc78
Tavo Annus <tavo.annus@gmail.com>
2021-11-28 21:16:56 +0200

    Add rofi launcher

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -203,7 +203,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "~/script/launcher.sh || dmenu")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
