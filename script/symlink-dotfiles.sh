69d51ccbc25c8765b047ee5c8650d534b03b5fc1
Tavo Annus <tavo.annus@gmail.com>
2022-01-05 21:28:55 +0200

    Add lightdm config

--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -1,12 +1,17 @@
-# ln -s -f ~/git/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s -v -f ~/git/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s -v -f ~/git/dotfiles/.config/procs/config.toml ~/.config/procs/config.toml
-ln -s -v -f ~/git/dotfiles/.config/starship/starship.toml ~/.config/starship.toml
-ln -s -v -f ~/git/dotfiles/.config/zsh/.zshrc ~/.config/zsh/.zshrc
-ln -s -v -f ~/git/dotfiles/.config/zsh/.zshenv ~/.zshenv
-ln -s -v -f ~/git/dotfiles/.config/xmobar/xmobarrc ~/.config/xmobar/xmobarrc
-ln -s -v -f ~/git/dotfiles/.config/xmobar/trayer-padding-icon.sh ~/.config/xmobar/trayer-padding-icon.sh
-ln -s -v -f ~/git/dotfiles/.config/rofi/ ~/.config/rofi
 
-ln -s -v -T -f ~/git/dotfiles/.xmonad ~/.xmonad
-ln -s -v -T -f ~/git/dotfiles/script ~/script
+gitdir=$(git rev-parse --show-toplevel)
+
+ln -s -v -f $gitdir/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s -v -f $gitdir/.config/procs/config.toml ~/.config/procs/config.toml
+ln -s -v -f $gitdir/.config/starship/starship.toml ~/.config/starship.toml
+ln -s -v -f $gitdir/.config/zsh/.zshrc ~/.config/zsh/.zshrc
+ln -s -v -f $gitdir/.config/zsh/.zshenv ~/.zshenv
+ln -s -v -f $gitdir/.config/xmobar/xmobarrc ~/.config/xmobar/xmobarrc
+ln -s -v -f $gitdir/.config/xmobar/trayer-padding-icon.sh ~/.config/xmobar/trayer-padding-icon.sh
+ln -s -v -f $gitdir/.config/rofi/ ~/.config/rofi
+
+ln -s -v -T -f $gitdir/.xmonad ~/.xmonad
+ln -s -v -T -f $gitdir/script ~/script
+
+# ln -s -v -f $gitdir/etc/pacman.conf /etc/pacman.conf
+# ln -s -v -f $gitdir/etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
