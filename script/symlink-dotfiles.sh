9fc570f534564f9ae4136511a1e8c8f868d9f91a
Tavo Annus <tavo.annus@gmail.com>
2022-01-15 11:22:18 +0200

    Add 1080p config

--- script/symlink-dotfiles.sh
+++ script/symlink-dotfiles.sh
@@ -6,8 +6,8 @@ ln -s -v -f $gitdir/.config/procs/config.toml ~/.config/procs/config.toml
 ln -s -v -f $gitdir/.config/starship/starship.toml ~/.config/starship.toml
 ln -s -v -f $gitdir/.config/zsh/.zshrc ~/.config/zsh/.zshrc
 ln -s -v -f $gitdir/.config/zsh/.zshenv ~/.zshenv
-ln -s -v -f $gitdir/.config/xmobar/xmobarrc ~/.config/xmobar/xmobarrc
-ln -s -v -f $gitdir/.config/xmobar/trayer-padding-icon.sh ~/.config/xmobar/trayer-padding-icon.sh
+
+ln -s -v -T -f $gitdir/.config/xmobar ~/.config/xmobar
 ln -s -v -T -f $gitdir/.config/rofi ~/.config/rofi
 
 ln -s -v -T -f $gitdir/.xmonad ~/.xmonad
