24e58bd6c7d4a01923a22ebe75d7a44a6aaebd7e
Tavo Annus <tavo.annus@gmail.com>
2023-03-25 22:00:22 +0200

    Various fixes

--- .steam/symlink.sh
+++ .steam/symlink.sh
@@ -1,3 +1,10 @@
-ln -s -f ~/git/dotfiles/.steam/csgo/autoexec.cfg ~/.steam/steam/userdata/119390087/730/local/cfg/autoexec.cfg
-ln -s -f ~/git/dotfiles/.steam/csgo/smokes.cfg ~/.steam/steam/userdata/119390087/730/local/cfg/smokes.cfg
+gitdir=$(git rev-parse --show-toplevel)
+
+ln -s -f $gitdir/.steam/csgo/autoexec.cfg ~/.steam/steam/userdata/119390087/730/local/cfg/autoexec.cfg
+ln -s -f $gitdir/.steam/csgo/smokes.cfg ~/.steam/steam/userdata/119390087/730/local/cfg/smokes.cfg
+
+ln -s --f $gitdir/.steam/tf2/autoexec.cfg ~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf/cfg/autoexec.cfg
+ln -s -f $gitdir/.steam/tf2/binds.cfg ~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf/cfg/binds.cfg
+ln -s -f $gitdir/.steam/tf2/crosshairs.cfg ~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf/cfg/crosshairs.cfg
+ln -s -f $gitdir/.steam/tf2/graphics.cfg ~/.steam/steam/steamapps/common/Team\ Fortress\ 2/tf/cfg/graphics.cfg
 
